e5f2e88845c4fbcb7be5b84b7b03c3904bff9cc0
kitety <aizaizuori@foxmail.com>
2022-04-28 23:14:29 +0800

    feat: set coin

---
+++ lib/sanity.ts
@@ -0,0 +1,10 @@
+import SanityClient from "@sanity/client";
+
+export const client = SanityClient({
+  projectId: "at4o6oea",
+  dataset: "production",
+  apiVersion: "2021-04-28",
+  token:
+    "skyxloK02D0nkqTsQG88OXq4wdCcs4lyOGfLX91maBdNbOV69bPFcn2GhCkDnGQfIDcIXwwifzKNiJYVZNdWUxXLJANkS3IfIzGuN6ZwOwVirvQOxIUtcNRVZGyKlwCvfjHteD7ReONR4w22UhK8TWoPye8oKice1z4L0kH8PDvBi172Db4K",
+  useCdn: false,
+});
