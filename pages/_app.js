9f28def38974e2cb132106474918e2bffdd50432
kitety <aizaizuori@foxmail.com>
2022-05-03 16:32:50 +0800

    feat: Portfolio

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "../styles/globals.css";
+
+const supportedChainIds = [ChainId.RINKEBY];
+const connectors = {
+  injected: {},
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
